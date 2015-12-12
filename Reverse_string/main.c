60d09aac02aff827ae322db95c302f53eda08252
yuga-92 <yurahalych@gmail.com>
2015-12-13 01:57:07 +0200

    Initial Commit

---
+++ Reverse_string/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Reverse_string
+//
+//  Created by Yuga Hal on 12/13/15.
+//  Copyright © 2015 Yuga Hal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

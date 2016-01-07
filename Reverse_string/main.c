72becb316c8b31f06ff0075548c474e9ddff28cb
yuga-92 <yurahalych@gmail.com>
2016-01-08 01:43:14 +0200

    Some comments removed

--- Reverse_string/main.c
+++ Reverse_string/main.c
@@ -5,11 +5,21 @@
 //  Created by Yuga Hal on 12/13/15.
 //  Copyright © 2015 Yuga Hal. All rights reserved.
 //
+//This program prints reverced string
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char ch[] = {"Hello String!"};
+    char temp;
+    for (int i=0; i< (sizeof(ch)/sizeof(ch[0]))/2; i++){
+        temp = ch[i];
+        ch[i]= ch[sizeof(ch)/sizeof(ch[0])-1-i];
+        ch[sizeof(ch)/sizeof(ch[0])-1-i]= temp;
+    }
+    for (int i=0; i< sizeof(ch)/sizeof(ch[0]);i++){
+        printf("%c", ch[i]);
+    }
+    printf("\n");
     return 0;
 }
